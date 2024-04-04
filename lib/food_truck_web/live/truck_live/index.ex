1b08ef5267e10d424b5ddd4889a1fabe7b0aa561
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2024-04-04 11:44:57 -0400

    Add code for search function

--- lib/food_truck_web/live/truck_live/index.ex
+++ lib/food_truck_web/live/truck_live/index.ex
@@ -44,4 +44,43 @@ defmodule FoodTruckWeb.TruckLive.Index do
 
     {:noreply, stream_delete(socket, :trucks, truck)}
   end
+
+  def handle_event("change", %{"search" => %{"query" => ""}}, socket) do
+    socket = assign(socket, :trucks, [])
+    {:noreply, socket}
+  end
+
+  def handle_event("change", %{"search" => %{"query" => search_query}}, socket) do
+    trucks = Trucks.search(search_query)
+    socket = assign(socket, :trucks, trucks)
+
+    {:noreply, socket}
+  end
+
+  def open_modal(js \\ %JS{}) do
+    js
+    |> JS.show(
+      to: "#searchbox_container",
+      transition:
+        {"transition ease-out duration-200", "opacity-0 scale-95", "opacity-100 scale-100"}
+    )
+    |> JS.show(
+      to: "#searchbar-dialog",
+      transition: {"transition ease-in duration-100", "opacity-0", "opacity-100"}
+    )
+    |> JS.focus(to: "#search-input")
+  end
+
+  def hide_modal(js \\ %JS{}) do
+    js
+    |> JS.hide(
+      to: "#searchbar-searchbox_container",
+      transition:
+        {"transition ease-in duration-100", "opacity-100 scale-100", "opacity-0 scale-95"}
+    )
+    |> JS.hide(
+      to: "#searchbar-dialog",
+      transition: {"transition ease-in duration-100", "opacity-100", "opacity-0"}
+    )
+  end
 end
